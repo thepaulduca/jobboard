e0d842fedca81300feb6cb73aae088db8f66a8fd
Paul Duca <paul.r.duca@gmail.com>
2016-10-24 15:08:05 -0400

    init commit

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,40 @@
+class JobsController < ApplicationController
+  before_action :find_job, only: [:edit, :update, :destroy]
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+  end
+
+  def edit
+  end
+
+  def update
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+  def find_job
+    @job = Job.find(params[:id])
+  end
+
+end
